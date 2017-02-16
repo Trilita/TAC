014ccdd9656cbc332b9420b69b5838187888d7e3
Javier Honduvilla Coto <javierhonduco@gmail.com>
2017-02-16 16:23:33 +0100

    new prime optimized

--- isprime.py
+++ isprime.py
@@ -12,20 +12,42 @@ def is_prime(number):
 
     return True
 
+def is_prime_v1(number):
+    if number == 0 or number == 1:
+        return False
+
+    if number == 2:
+        return True
+
+    if number % 2 == 0:
+        return False
+
+    i = 3
+    while i < number-1:
+        if number%i == 0:
+            return False
+        i += 2
+
+    return True
+
 # comprueba que nuestra implementación es correcta
 # usando la librería de cálculo simbólico `sympy`
 # y contrastando ambos resultados
-def check(until=100):
+def check(function, until=10*1000):
     import sympy
 
     for i in range(0, until):
-        if sympy.isprime(i) != is_prime(i):
-            print("don't match")
-            print(i)
+        if sympy.isprime(i) != function(i):
+            print("doesn't match")
+            print(i, sympy.isprime(i), function(i))
             sys.exit(-1)
 
+    print('everything ok :)')
+
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('usage: python isprime.py <number>')
-        sys.exit(-1)
-    print(is_prime(int(sys.argv[1])))
+    check(is_prime_v1)
+
+    #if len(sys.argv) < 2:
+    #    print('usage: python isprime.py <number>')
+    #    sys.exit(-1)
+    #print(is_prime(int(sys.argv[1])))
