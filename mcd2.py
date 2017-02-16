bfbb9b67dc4383ea7dab855bea7a32fcc46cf236
100315115 <100315115@alumnos.uc3m.es>
2017-02-16 15:21:20 +0100

    Euclides second version

--- mcd2.py
+++ mcd2.py
@@ -1,19 +1,19 @@
 import sys
 
 
-def mcd2(number1, number2):
-    if number1 == number2:
-        return 1
-    else:
-        current_dividend = max(number1, number2)
-        current_dividing = min(number1, number2)
-        while True:
-            current_remainder = current_dividend % current_dividing
-            if current_remainder == 0:
-                return current_dividing
-            else:
-                current_dividend = current_dividing
-                current_dividing = current_remainder
+def Euclides(number1, number2):
+	if number1 == number2:
+		return number1
+	else:
+		current_dividend = max(number1,number2)
+		current_dividing = min(number1,number2) 
+		while True:
+			current_remainder = current_dividend % current_dividing
+			if current_remainder == 0:
+			 	return current_dividing
+			else:
+				current_dividend = current_dividing
+				current_dividing = current_remainder
 
 
 if __name__ == '__main__':
@@ -21,4 +21,4 @@ if __name__ == '__main__':
         print('usage: python mcd1.py <num1> <num2>')
         sys.exit(-1)
 
-    print(mcd2(int(sys.argv[1]), int(sys.argv[2])))
+print(Euclides(int(sys.argv[1]), int(sys.argv[2])))
