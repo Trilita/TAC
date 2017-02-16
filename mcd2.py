5cbc6ac6d7947229553fabf13684af721698b2aa
100315115 <100315115@alumnos.uc3m.es>
2017-02-16 12:50:18 +0100

    mdc2 Done

---
+++ mcd2.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def Euclides(number1, number2):
+	if number1 == number2:
+		return 1
+	else:
+		current_dividend = max(number1,number2)
+		current_dividing = min(number1,number2) 
+		while True:
+			current_remainder = current_dividend % current_dividing
+			if current_remainder == 0:
+			 	return current_dividing
+			else:
+				current_dividend = current_dividing
+				current_dividing = current_remainder
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('usage: python mcd1.py <num1> <num2>')
+        sys.exit(-1)
+
+print(Euclides(int(sys.argv[1]), int(sys.argv[2])))
